0d9598bfa32971d75f036032cbbd35be8a08e1e4
Fernanda Gonçalves <fernanda-bess@hotmail.com>
2023-10-14 16:37:39 -0300

    cirando model de complemento

---
+++ src/models/Complementos.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Complementos extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Complementos.init({
+    comp_numero: DataTypes.STRING,
+    comp_complemento: DataTypes.STRING,
+    comp_bairro: DataTypes.STRING,
+    comp_ponto_referencia: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'Complementos',
+  });
+  return Complementos;
+};
