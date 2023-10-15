0e2a6ff6fb8396d0910e7791d7147e3f02769742
briitogabriel <gabriel.brito.a@hotmail.com>
2023-10-15 19:30:56 -0300

    alterei o server para POO

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-require("dotenv").config();
-const PORT = process.env.APP_PORT;
-const { server } = require("./server");
-const { Sequelize } = require("sequelize");
-const DB_CONFIG = require("./config/database");
+const { Server } = require('./server')
 
-const sequelize = new Sequelize(DB_CONFIG);
-const connect = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Conexão com banco de dados bem sucedida");
-  } catch (err) {
-    console.log("Sem conexão com banco de dados", err);
-  }
-};
-
-connect();
-
-server.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+new Server()
--- src/server.js
+++ src/server.js
@@ -1,20 +1,49 @@
 require("dotenv").config();
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const express = require("express");
 const cors = require("cors");
-const server = express();
 
-server.use(
-  cors({
-    origin: "*",
-  })
-);
-server.use(express.json());
+const { Sequelize } = require("sequelize");
+const DB_CONFIG = require("./config/database");
+const sequelize = new Sequelize(DB_CONFIG);
 
-server.get("/", (request, response) => {
-  response.status(200).send({message: `${process.env.APP_NAME} está no ar!`});
-});
+const routes = require('./routes')
 
-module.exports = {
-  server,
-};
+class Server {
+  constructor ( server = express())
+  {
+    this.middlewares(server)
+    this.database()
+    this.allRoutes(server)
+    this.initializeServer(server)
+  }
+
+  async middlewares(app) {
+    app.use(
+      cors({
+        origin: "*",
+      })
+    );
+    app.use(express.json());
+  }
+
+  async database(){
+    try {
+      await sequelize.authenticate();
+      console.log("Conexão com banco de dados bem sucedida");
+    } catch (err) {
+      console.log("Sem conexão com banco de dados", err);
+    }
+  }
+  
+    async allRoutes(app) {
+      app.use(routes)
+    }
+
+  async initializeServer(app){
+    const PORT = process.env.APP_PORT
+    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+  }
+}
+
+module.exports = { Server }
