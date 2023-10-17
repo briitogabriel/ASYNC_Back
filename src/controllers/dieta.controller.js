6a4c76693355a047f8106046a90a614ac139790b
briitogabriel <gabriel.brito.a@hotmail.com>
2023-10-16 21:31:37 -0300

    testes validacoes

--- src/controllers/dieta.controller.js
+++ src/controllers/dieta.controller.js
@@ -3,6 +3,7 @@ const DB_CONFIG = require("../config/database");
 const sequelize = new Sequelize(DB_CONFIG);
 const Dietas = require('../models/Dietas')(sequelize, Sequelize);
 
+const tipos_dieta = ['Low Carb', 'Dash', 'Paleolítica', 'Cetogênica', 'Dukan', 'Mediterrânea', 'Outra']
 class DietaController {
     async create(req, res) {
         try {
@@ -14,23 +15,41 @@ class DietaController {
                 die_descricao,
                 pac_id,
             } = req.body
+
+            if (
+                die_nome.length >= 4
+                && die_nome.length <= 100
+                && die_data
+                && die_hora
+                && tipos_dieta.includes(die_tipo)
+                && die_descricao
+                && pac_id > 0
+            ) 
+            {
+                console.log(true)
+                const dietaCreated = await Dietas.create({
+                    die_nome,
+                    die_data,
+                    die_hora,
+                    die_tipo,
+                    die_descricao,
+                    pac_id,
+                    created_at: new Date(),
+                    updated_at: new Date()
+                })
+                
+                return res.status(201).send(dietaCreated)
+            }
             
-            const dietaCreated = await Dietas.create({
-                die_nome,
-                die_data,
-                die_hora,
-                die_tipo,
-                die_descricao,
-                pac_id,
-                created_at: new Date(),
-                updated_at: new Date()
+            console.log(false)
+            return res.status(400).send({
+                message: "Preencha os campos obrigatórios"
             })
 
-            return res.status(201).send(dietaCreated)
         } catch (error) {
-            return res.status(400).send({
+            return res.status(500).send({
                 message: "Erro na criação da Dieta",
-                cause: error.message
+                cause: error//.message
             })
         }
     }
