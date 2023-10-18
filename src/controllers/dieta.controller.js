c969d019d6eb432272378fc118c27c2b4e797c60
briitogabriel <gabriel.brito.a@hotmail.com>
2023-10-17 21:06:51 -0300

    validações dos campos de dieta

--- src/controllers/dieta.controller.js
+++ src/controllers/dieta.controller.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require("sequelize");
 const DB_CONFIG = require("../config/database");
 const sequelize = new Sequelize(DB_CONFIG);
-const Dietas = require('../models/Dietas')(sequelize, Sequelize);
 
-const tipos_dieta = ['Low Carb', 'Dash', 'Paleolítica', 'Cetogênica', 'Dukan', 'Mediterrânea', 'Outra']
+const dietaSchema = require("../validations/dietaValidation");
+const Dietas = require('../models/Dietas')(sequelize, Sequelize);
 class DietaController {
     async create(req, res) {
         try {
@@ -16,40 +16,32 @@ class DietaController {
                 pac_id,
             } = req.body
 
-            if (
-                die_nome.length >= 4
-                && die_nome.length <= 100
-                && die_data
-                && die_hora
-                && tipos_dieta.includes(die_tipo)
-                && die_descricao
-                && pac_id > 0
-            ) 
-            {
-                console.log(true)
-                const dietaCreated = await Dietas.create({
-                    die_nome,
-                    die_data,
-                    die_hora,
-                    die_tipo,
-                    die_descricao,
-                    pac_id,
-                    created_at: new Date(),
-                    updated_at: new Date()
-                })
-                
-                return res.status(201).send(dietaCreated)
-            }
+            await dietaSchema.validate(
+                {
+                  die_nome,
+                  die_data,
+                  die_hora,
+                  die_tipo,
+                  die_descricao,
+                }
+              );
             
-            console.log(false)
-            return res.status(400).send({
-                message: "Preencha os campos obrigatórios"
+            const dietaCreated = await Dietas.create({
+                die_nome,
+                die_data,
+                die_hora,
+                die_tipo,
+                die_descricao,
+                pac_id,
+                created_at: new Date(),
+                updated_at: new Date()
             })
+            return res.status(201).send(dietaCreated)
 
         } catch (error) {
             return res.status(500).send({
                 message: "Erro na criação da Dieta",
-                cause: error//.message
+                cause: error.message
             })
         }
     }
--- src/routes/dietas.routes.js
+++ src/routes/dietas.routes.js
@@ -1,7 +1,6 @@
-// const { create, findAll, findOne, login, findCarts, update, updatePassword } = require('../controllers/user.controller')
 const { Router } = require('express');
 const { create } = require('../controllers/dieta.controller');
-// const { auth } = require('../middlewares/auth.middleware')
+const { auth } = require('../middlewares/auth.middleware');
 
 class DietaRouter{
     routesFromDietas() {
@@ -9,13 +8,7 @@ class DietaRouter{
         dietaRoutes.get('/', (request, response) => {
             response.status(200).send({message: `${process.env.APP_NAME} está no ar!`});
             });
-        dietaRoutes.post('/dietas', create)
-        // dietaRoutes.get('/users', auth , findAll)
-        // dietaRoutes.get('/users/:userId', auth, findOne)
-        // dietaRoutes.patch('/users/:userId', auth, update)
-        // dietaRoutes.patch('/users/:userId/change-password', auth, updatePassword)
-        // dietaRoutes.post('/users/login', login)
-        // dietaRoutes.get('/users/:userId/carts', findCarts)
+        dietaRoutes.post('/dietas', auth, create)
 
         return dietaRoutes
     }
