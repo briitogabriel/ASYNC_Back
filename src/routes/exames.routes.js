645cee75c998e17dffa89dd88f67bf64729f480d
Taelen Machado <taelen_machado@estudante.sesisenai.org.br>
2023-10-29 00:38:49 -0300

    Ajustes na api de exames

--- src/routes/exames.routes.js
+++ src/routes/exames.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
-// implementar a pacienteByExame ==> const { create, findAllByPatient, update, remove, findAllAdmin } = require('../controllers/exames.controller');
+// implementar a pacienteByExame ==> 
+const { create, findAllByPatient, update, remove, findAllAdmin, findOne } = require('../controllers/exames.controller');
 const { auth } = require('../middlewares/auth.middleware');
 
 class ExamesRouter{
@@ -7,9 +8,10 @@ class ExamesRouter{
         const exameRoutes = Router()
         exameRoutes.post('/exames', auth, create)
         // implementar a rota de pacienteByExame ==> exameRoutes.get('/dietas', auth, findAllByPatient)
-        exameRoutes.put('/exames/:exameId', auth, update)
-        exameRoutes.delete('/exames/:exameId', auth, remove)
-        exameRoutes.get('/exames/admin', auth, findAllAdmin)
+        exameRoutes.put('/exames/:exameId',auth, update)
+        exameRoutes.get('/exames/:exameId',auth, findOne)
+        exameRoutes.delete('/exames/:exameId',auth, remove)
+        exameRoutes.get('/exames/admin',auth, findAllAdmin)
 
         return exameRoutes
     }
--- src/routes/prontuarios.routes.js
+++ src/routes/prontuarios.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
 const { auth } = require("../middlewares/auth.middleware");
-const { create, update, findAll, findOne, remove } = require("../controllers/paciente.controller");
+const { findAll } = require("../controllers/prontuarios.controller");
 
 class ProntuariosRouter {
   routesFromProntuario() {
     const prontuarioRoutes = Router();
-    prontuarioRoutes.get("/prontuarios", auth, findAll);
-    return ProntuariosRouter;
+    prontuarioRoutes.get("/prontuarios", findAll);
+    return prontuarioRoutes;
   }
 }
 
