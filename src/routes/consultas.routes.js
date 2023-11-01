0739d60e3d9ee123b17dc47986ba16bc1833456d
AnjaClara <anaclaraguimaraes1@gmail.com>
2023-10-31 22:03:48 -0300

    rotas de atualização, delete e retorno

--- src/routes/consultas.routes.js
+++ src/routes/consultas.routes.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
-const { create, findAll } = require('../controllers/consultas.controller');
+const { create, update, findAllAdmin, findOne, findAllByPatient, remove } = require('../controllers/consultas.controller');
 const { auth } = require('../middlewares/auth.middleware');
 
 class ConsultasRouter{
   routesFromConsultas(){
     const consultaRoutes = Router()
     consultaRoutes.post('/consultas', auth, create)
-    consultaRoutes.get("/consultas", auth, findAll);
+    consultaRoutes.put('/consultas/:consultaId', auth, update)
+    consultaRoutes.get('/consultas/:consultaId', auth, findOne)
+    consultaRoutes.get('/consultas/', auth, findAllAdmin)
+    consultaRoutes.get('/pacientes/:pacienteId/consultas', auth, findAllByPatient)
+    consultaRoutes.delete('consultas/:consultaId', auth, remove)
 
     return consultaRoutes
   }
