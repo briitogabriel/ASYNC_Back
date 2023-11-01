c99baeda70665114b78c8c21502948d7154af93c
Ana Paula Dos Santos Marianni <125224689+anaPaulaSantosM@users.noreply.github.com>
2023-10-31 21:47:56 -0300

    byUser exames.routes.js

--- src/routes/exames.routes.js
+++ src/routes/exames.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { auth } = require('../middlewares/auth.middleware');
-const { create, update, remove, findAllAdmin, findOne, findAllByPatient } = require("../controllers/exames.controller");
+const { create, update, remove, findAllAdmin, findOne, findExamesByUser } = require("../controllers/exames.controller");
 
 class ExamesRouter{
     routesFromExames() {
@@ -10,7 +10,7 @@ class ExamesRouter{
         exameRoutes.get('/exames/:exameId',auth, findOne)
         exameRoutes.delete('/exames/:exameId',auth, remove)
         exameRoutes.get('/exames/admin',auth, findAllAdmin)
-        exameRoutes.get('/pacientes/:pacienteId/exames',auth, findAllByPatient)
+        exameRoutes.get('/exames',auth, findExamesByUser)
 
         return exameRoutes
     }
