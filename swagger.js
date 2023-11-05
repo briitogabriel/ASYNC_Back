4954ce6b8f0b919f5e95c1739e55c71a6b68077c
briitogabriel <gabriel.brito.a@hotmail.com>
2023-11-04 21:56:44 -0300

    swagger tags e dotenv

--- swagger.js
+++ swagger.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const swaggerAutogen = require('swagger-autogen')()
 
 const outputFile = './swagger_output.json'
@@ -5,66 +6,30 @@ const endpointsFiles = ['./src/routes/modules/*.js']
 
 const docDefinition = {
   info: {
-      version: "1.0.0",
-      title: "ASYNC Lab API",
-      description: "Documentação da <b>SQUAD4 - ASYNC</b>, gerada automaticamente com <b>swagger-autogen</b>"
+    version: "1.0.0",
+    title: "ASYNC Lab API",
+    description: "Documentação da <b>SQUAD4 - ASYNC</b>, gerada automaticamente com <b>swagger-autogen</b>"
   },
-  host: "localhost:3333",
+  host: `localhost:${process.env.APP_PORT}`,
   basePath: "/api/",
   schemes: ['http', 'https'],
   consumes: ['application/json'],
   produces: ['application/json'],
-  // tags: [
-  //     {
-  //         "name": "User",
-  //         "description": "Endpoints"
-  //     }
-  // ],
-  // securityDefinitions: {
-  //     api_key: {
-  //         type: "apiKey",
-  //         name: "api_key",
-  //         in: "header"
-  //     },
-  //     petstore_auth: {
-  //         type: "oauth2",
-  //         authorizationUrl: "https://petstore.swagger.io/oauth/authorize",
-  //         flow: "implicit",
-  //         scopes: {
-  //             read_pets: "read your pets",
-  //             write_pets: "modify pets in your account"
-  //         }
-  //     }
-  // },
-  // definitions: {
-  //     Parents: {
-  //         father: "Simon Doe",
-  //         mother: "Marie Doe"
-  //     },
-  //     User: {
-  //         name: "Jhon Doe",
-  //         age: 29,
-  //         parents: {
-  //             $ref: '#/definitions/Parents'
-  //         },
-  //         diplomas: [
-  //             {
-  //                 school: "XYZ University",
-  //                 year: 2020,
-  //                 completed: true,
-  //                 internship: {
-  //                     hours: 290,
-  //                     location: "XYZ Company"
-  //                 }
-  //             }
-  //         ]
-  //     },
-  //     AddUser: {
-  //         $name: "Jhon Doe",
-  //         $age: 29,
-  //         about: ""
-  //     }
-  // }
+  tags: [
+    { name: "Usuários", description: "Endpoints de requisições de Usuários" },
+    { name: "Consultas", description: "Endpoints de requisições de Consultas" },
+    { name: "Dietas", description: "Endpoints de requisições de Dietas" },
+    { name: "Estatísticas", description: "Endpoints de requisições de Estatísticas" },
+    { name: "Exames", description: "Endpoints de requisições de Exames" },
+    { name: "Exercícios", description: "Endpoints de requisições de Exercícios" },
+    { name: "Logs", description: "Endpoints de requisições de Logs" },
+    { name: "Medicamentos", description: "Endpoints de requisições de Medicamentos" },
+    { name: "Pacientes", description: "Endpoints de requisições de Pacientes" },
+    { name: "Prontuários", description: "Endpoints de requisições de Prontuários" },
+  ],
+  definitions: {
+    
+  }
 }
 
 swaggerAutogen(outputFile, endpointsFiles, docDefinition)
